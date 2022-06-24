bbdd15a1ab2a4f6073648bd99ad16271408a78c5
Azzam Jihad Ulhaq <azzam.jihad@moladin.com>
2022-06-24 00:58:54 +0000

    connect to database

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-  for {
-    fmt.Println("Hello World")
-    time.Sleep(time.Second * 3)
-  }
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(host.docker.internal:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database is good")
 }
